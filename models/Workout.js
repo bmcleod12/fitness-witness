42bf6ceafa959186e4ebf54d26e7974c83752cce
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-04-13 20:39:34 -0500

    added models based on seed file to capture each possible field

---
+++ models/Workout.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [{
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter a type of exercise"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter the name of the exercise"
+      },
+      duration: {
+        type: Number,
+        required: "Enter the duration of the exercise"
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      }
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
